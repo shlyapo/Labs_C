8992b1060770cb2ab2e007cb285f256fc5d7944b
shlyapo <72437851+shlyapo@users.noreply.github.com>
2021-04-28 17:26:52 +0300

    Delete laba_2_1(c) directory

--- laba_2_1(c)/laba_2_1.h
+++
@@ -1,8 +0,0 @@
-void Decision(int cost, int trueCost, int costCountry, int counter);
-int Parameters(int countCement, int countGravel, int countConcrete, int cost);
-int GetContry();
-int Cement();
-int Gravel();
-int Concrete();
-void Info();
-void Exit();
